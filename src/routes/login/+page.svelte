b265d18792c39fc698c82e2932050c2f25fb3c3a
dhnpx <122553352+dhnpx@users.noreply.github.com>
2024-05-08 16:02:25 -0700

    fixed layout

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,7 +5,7 @@
     <title>WorkCoffee Login</title>
 </head>
 <body>
-    <link rel="stylesheet" href="registerStyle.css">
+    <link rel="stylesheet" href="loginStyle.css">
     <div class="Top">
         <div class = "navbar">
             <div class = "background"></div>
@@ -19,42 +19,32 @@
                 </div> 
             </button>
         </div>
-        <div class = "emptySpace"></div>
-        <div class = "registerSection">
-            <div class = "Register">
-                <h1>Register</h1>
+
+        <div class = "loginSection">
+            <div class = "Login">
+                <h1>Login</h1>
             </div>
             <div class = "user">
                 <input type="text" required>
-                <div class = "inputUser">Input Username</div>
-            </div>
-            <div class = "email">
-                <input type = "email" required>
-                <div class = "inputEmail">Input Email</div>
+                <div class = "inputUser">Input Username or Email</div>
             </div>
             <div class = "passWord">
                 <input type = "text" required>
                 <div class = "inputPass">Input Password</div>
             </div>
-            <div class = "rePassword">
-                <input type = "text" required>
-                <div class="inputRePass">Confirm Password</div>
-            </div>
             <div class = "accButtons">
-                <button class = "createAccount">
-                    <h3>Register</h3>
-                </button>
-                <p>Or, if you have an account:</p>
-                <button onclick="location.href='loginFrame.html'" class = "loginButton">
+                
+                <button class = "loginButton">
                     <h3>Login</h3>
                 </button>
-                
+                <p>Or, if you have want to make an account:</p>
+                <button on:click={() => location.href='/register'} class = "createAccount">
+                    <h3>Register</h3>
+                </button>
             </div>
         </div>
-        <div class = "emptySpace"></div>
     </div>
 </body>
-
 <style>
 html, body{
     height: 100%;
@@ -72,7 +62,6 @@ body{
     background-position: center;
     height: 100%;
     font-family: 'Inter';
-    gap: 6em;
     }
     
     /* Nav Bar */
@@ -97,8 +86,7 @@ body{
     .Top .emptySpace{
         background-color: #ffffff;
         width: 100%;
-        height: 5%;
-        position: absolute;
+        height: 100%;
     }
     /*Contents*/
     
@@ -146,20 +134,20 @@ body{
     
     /* Login */
     
-    .Top .registerSection{
+    .Top .loginSection{
        position: relative; 
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        height: 100%;
+        height: 80%;
         gap: 6em;
     }
 
-    .Top .registerSection .user,
-    .Top .registerSection .email,
-    .Top .registerSection .passWord,
-    .Top .registerSection .rePassword{
+    .Top .loginSection .user,
+    .Top .loginSection .email,
+    .Top .loginSection .passWord,
+    .Top .loginSection .rePassword{
         position: relative;
         height: 120%;
         width: 50%;
@@ -167,18 +155,14 @@ body{
         align-items: center;
         justify-content: center;
     }
-    .Top .registerSection .user,
-    .Top .registerSection .email,
-    .Top .registerSection .passWord,
-    .Top .registerSection .rePassword h3{
+    .Top .loginSection .user,
+    .Top .loginSection .passWord{
         color: #8B6504;
         font-size: 1em;
     }
 
-    .Top .registerSection .user input,
-    .Top .registerSection .email input,
-    .Top .registerSection .passWord input,
-    .Top .registerSection .rePassword input{
+    .Top .loginSection .user input,
+    .Top .loginSection .passWord input{
         position: absolute;
         display: flex;
         align-items: center;
@@ -195,10 +179,8 @@ body{
         margin: auto;
     }
 
-    .Top .registerSection .user .inputUser,
-    .Top .registerSection .email .inputEmail,
-    .Top .registerSection .passWord .inputPass,
-    .Top .registerSection .rePassword .inputRePass{
+    .Top .loginSection .user .inputUser,
+    .Top .loginSection .passWord .inputPass{
         position: absolute;
         color: #696969;
         font-size: 1.2em;
@@ -209,33 +191,25 @@ body{
 
     /*hoo boy (Text and box transition effect on click and ready)*/
 
-    .Top .registerSection .user input:focus,
-    .Top .registerSection .email input:focus,
-    .Top .registerSection .passWord input:focus,
-    .Top .registerSection .rePassword input:focus,
-    .Top .registerSection .user input:valid,
-    .Top .registerSection .email input:valid,
-    .Top .registerSection .passWord input:valid,
-    .Top .registerSection .rePassword input:valid{
+    .Top .loginSection .user input:focus,
+    .Top .loginSection .passWord input:focus,
+    .Top .loginSection .user input:valid,
+    .Top .loginSection .passWord input:valid{
         color: #8B6504;
         border: 3px solid #8b6504;
     }
 
-    .Top .registerSection .user input:focus + .inputUser,
-    .Top .registerSection .email input:focus + .inputEmail,
-    .Top .registerSection .passWord input:focus + .inputPass,
-    .Top .registerSection .rePassword input:focus + .inputRePass,
-    .Top .registerSection .user input:valid + .inputUser,
-    .Top .registerSection .email input:valid + .inputEmail,
-    .Top .registerSection .passWord input:valid + .inputPass,
-    .Top .registerSection .rePassword input:valid + .inputRePass{
+    .Top .loginSection .user input:focus + .inputUser,
+    .Top .loginSection .passWord input:focus + .inputPass,
+    .Top .loginSection .user input:valid + .inputUser,
+    .Top .loginSection .passWord input:valid + .inputPass{
         color: #8B6504;
         transform: translate(0, -1.6em) scale(0.8);
         z-index: 10000;
     }
     /*Login and Register buttons*/
 
-    .Top .registerSection .accButtons{
+    .Top .loginSection .accButtons{
         margin-top: 50px;
         display: flex;
         flex-direction: column;
@@ -245,11 +219,11 @@ body{
         width: 50%;
         gap: 10px;
     }
-    .Top .registerSection .accButtons p{
+    .Top .loginSection .accButtons p{
         color: #969696;
         height: 0.25em;
     }
-    .Top .registerSection .accButtons .loginButton{
+    .Top .loginSection .accButtons .loginButton{
         height: 80%;
         width: 40%;
         background-color: rgb(255, 255, 255);
@@ -259,7 +233,7 @@ body{
         z-index: 4;
         transition: 0.2s ease;
     }
-    .Top .registerSection .accButtons .createAccount{
+    .Top .loginSection .accButtons .createAccount{
         height: 80%;
         width: 40%;
         background-color: black;
@@ -270,20 +244,20 @@ body{
         transition: 0.2s ease;
     }
 
-    .Top .registerSection .accButtons .loginButton:hover{
+    .Top .loginSection .accButtons .loginButton:hover{
         color: #ffffff;
         background-color: #8B6504;
         border-color: #8B6504;
         cursor:pointer;
     }
-    .Top .registerSection .accButtons .createAccount:hover{
+    .Top .loginSection .accButtons .createAccount:hover{
         color: #8B6504;
         background-color: #ffffff;
         border-color: #8B6504;
         cursor: pointer;
     }
-    .Top .registerSection .accButtons .loginButton,
-    .Top .registerSection .accButtons .createAccount{
+    .Top .loginSection .accButtons .loginButton,
+    .Top .loginSection .accButtons .createAccount{
         display: flex;
         justify-content: center; 
         align-items: center; 
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<body> 
+    <h1>Profile</h1>
+    <div>insert photo here</div>
+</body>
+
+<style>
+</style>
