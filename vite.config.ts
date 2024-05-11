06e045efd3570299d921b1977aa84db5b8347ec7
dhnpx <122553352+dhnpx@users.noreply.github.com>
2024-05-11 07:41:59 -0700

    revert

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
-/** @type {import('vite').UserConfig} */
-const config = {
-	plugins: [
-		sveltekit(),
-		SvelteKitPWA({
-			srcDir: "src",
-			filename: "my-sw.js",
-		})
-	],
-};
-
-export default config;
+export default defineConfig({
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
